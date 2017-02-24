59476ac7ac81ff7f60aaa93f54efbed51fa79072
cpari <cpari@DESKTOP-BEDGVAV>
2017-02-24 11:37:19 +0100

    Uitgebreid

--- Azure/Labs/Resource Groups/Resource-Groups.ps1
+++ Azure/Labs/Resource Groups/Resource-Groups.ps1
@@ -1,5 +1,8 @@
-#Login to Azure Account
+﻿#Login to Azure Account
 Login-AzureRmAccount
+
+Get-AzureRmSubscription
+Select-AzureRmSubscription -SubscriptionId 58ab0f56-3b9e-4ce9-bc15-7fcc4b98012c
 
 $locName = "West Europe" #Azure datacenter location
 $rgName = "testrg1" #resource group name
@@ -12,6 +15,7 @@ New-AzureRmResourceGroup -Name $rgName -Location $locName
 
 #View resource providers
 Get-AzureRmResourceProvider -Location $locName -ListAvailable | Select ProviderNameSpace,RegistrationState | Sort ProviderNameSpace
+Get-AzureRmResourceProvider -Location $locName | Register-AzureRmResourceProvider
 
 #Delete resource groups
 Remove-AzureRmResourceGroup -Name $rgName -Force
